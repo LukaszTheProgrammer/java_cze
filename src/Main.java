0025a5dce8a4cb33ceb0bb9565d01c88ff67f996
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-08 20:34:51 +0100

    adding comments

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
-
 class Auto{
-    int predkosc;
-    double przyspieszenie;
-    double pojemnosc;
+    int predkosc; // 0 by default
+    double przyspieszenie; // 0.0 by default
+    double pojemnosc; // 0.0  by default
+}
+
+class Dummy {
+    boolean b; // false by default
+    char c; // 0x0000 by default
+    String s; // null by default
+    Punkt p; // null by default
+    double d; // 0.0 by default
+}
+
+class Punkt {
+    int x; // 0 by default
+    int y; // 0 by default
 }
 
 public class Main {
@@ -20,6 +32,12 @@ public class Main {
         mercedes.pojemnosc= 2.5;
 
         System.out.println(bmw.predkosc);
+        System.out.println(bmw.przyspieszenie);
+        System.out.println(bmw.pojemnosc);
+
+        System.out.println(mercedes.predkosc);
+        System.out.println(mercedes.przyspieszenie);
+        System.out.println(mercedes.pojemnosc);
 
         Punkt p = new Punkt();
         p.x=15;
@@ -27,7 +45,7 @@ public class Main {
         System.out.println(p.y);
 
         Dummy dummy = new Dummy(); //tworzymy obiekt typu Dummy
-        //który posiada 5 pól, żadnemu z pól nie nadaliśmy domyślnych wartości
+        //który posiada 5 pól, żadnemu z pól nie nadaliśmy wartości
         //dlatego w poniższych liniach zobaczymy wartości domyślne zależne od typu zmiennych
         System.out.println(dummy.b);
         System.out.println(dummy.c);
@@ -37,19 +55,4 @@ public class Main {
 
         Dummy d2; // Deklaracja obiektu dummy - nic nie tworzymy - wartość d2 jest null
     }
-
 }
-
-class Dummy {
-    boolean b; // false by default
-    char c; // 0x0000 by default
-    String s; // null by default
-    Punkt p; // null by default
-    double d; // 0.0 by default
-}
-
-class Punkt {
-    int x;
-    int y;
-}
-
