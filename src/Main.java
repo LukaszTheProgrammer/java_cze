9c0e00a305e578ea957f9a06eb2aeabb7593e6b1
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-08 20:43:51 +0100

    account example

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
+
 public class Main {
 
     public static void main(String[] args) {
+        Osoba osoba = new Osoba("Tomek", "Nowak");
+        Konto eKonto = new Konto(1500.00, osoba, 1.5);
 
-        System.out.println("Hello World!");
+        System.out.println(eKonto);
     }
 }
 
