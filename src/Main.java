d2f4f6448bd70719d552a5192e19326eb9436258
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-09 10:23:08 +0100

    static variables

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
-class Punkt {
+class Demo {
 
-    int x;
-    int y;
+    static int licznik = 0;
+    int x = 0;
+
+    public Demo() {
+        licznik++;
+        x++;
+    }
 }
 
 public class Main {
 
-    public static void main(String[] args)
-        int "a" = 15;
-        punkt p = new punkt();
-        System.out.println(a)
+    public static void main(String[] args) {
+        Demo d1 = new Demo();
+        System.out.println(Demo.licznik+ " "+d1.x);
+        Demo d2 = new Demo();
+        System.out.println(Demo.licznik+ " "+d2.x);
+        Demo d3 = new Demo();
+        System.out.println(Demo.licznik+ " "+d3.x);
+        Demo d4 = new Demo();
+        System.out.println(Demo.licznik+ " "+d4.x);
+
     }
 }
 
