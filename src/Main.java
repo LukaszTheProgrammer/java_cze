b1595b35f44453180c48034c4afd4d0cb7868c6a
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-09 11:36:42 +0100

    parzystosc

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,21 @@
-class Demo {
-
-    static int licznik = 0;
-    int x = 0;
-
-    public Demo() {
-        licznik++;
-        x++;
-    }
-}
 
 public class Main {
 
     public static void main(String[] args) {
-        // 2 -> 4
-        // 3 -> 6
-        // 4 -> 8
+        int a = Matematyka.wartoscBezwzgledna(120);
+        int b = Matematyka.wartoscBezwzgledna(-300);
+        int c = Matematyka.wartoscBezwzgledna(-500);
 
-        int wynik = podwajaj(16);  // 32
-        System.out.println(wynik);
+        System.out.println(a); // 120
+        System.out.println(b); // 300
+        System.out.println(c); // 500
+
+        boolean nieparzysta = Matematyka.czyParzysta(123);
+        boolean parzysta = Matematyka.czyParzysta(250);
+
+        System.out.println(nieparzysta); // false
+        System.out.println(parzysta);  // true
+
     }
-
-    static int podwajaj(int liczba){
-        return liczba * 2;
-    }
-
-
 }
 
