def5e75d2c2e75d39f6fccb999abbb8e78e88773
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-09 12:03:36 +0100

    system-in

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
+import java.util.Date;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int a = Matematyka.wartoscBezwzgledna(120);
-        int b = Matematyka.wartoscBezwzgledna(-300);
-        int c = Matematyka.wartoscBezwzgledna(-500);
+        Scanner scanner = new Scanner(System.in);
+        String polecenie = scanner.nextLine();
 
-        System.out.println(a); // 120
-        System.out.println(b); // 300
-        System.out.println(c); // 500
-
-        boolean nieparzysta = Matematyka.czyParzysta(123);
-        boolean parzysta = Matematyka.czyParzysta(250);
-
-        System.out.println(nieparzysta); // false
-        System.out.println(parzysta);  // true
-
+        if (polecenie.equals("czas")) {
+            System.out.println(new Date());
+        } else if (polecenie.equals("newLine")) {
+            System.out.println("");
+        } else if (polecenie.equals("liczbaLosowa")) {
+            long seed = new Date().getTime();
+            int x = new Random(seed).nextInt();
+            System.out.println(x);
+        } else {
+            System.out.println("Nieobslugiwane polecenie!");
+        }
     }
 }
 
