550ceff8fef5035f843c967ca12ba2e3c39aef03
LukaszTheProgrammer <lukasztheprogrammer@gmail.com>
2018-12-09 12:24:36 +0100

    kalkulator

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,17 @@
-import java.util.Date;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String polecenie = scanner.nextLine();
+        int operand1 = scanner.nextInt();
+        String operacja = scanner.next();
+        int operand2 = scanner.nextInt();
 
-        if (polecenie.equals("czas")) {
-            System.out.println(new Date());
-        } else if (polecenie.equals("newLine")) {
-            System.out.println("");
-        } else if (polecenie.equals("liczbaLosowa")) {
-            long seed = new Date().getTime();
-            int x = new Random(seed).nextInt();
-            System.out.println(x);
+        if (operacja.equals("+")) {
+            System.out.println(operand1 + operand2);
+        } else if (operacja.equals("*")) {
+            System.out.println(operand1 * operand2);
         } else {
             System.out.println("Nieobslugiwane polecenie!");
         }
