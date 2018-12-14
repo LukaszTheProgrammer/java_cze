c9ba8216f4879f4eca3037a4069c812910b0153d
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-14 11:47:40 +0100

    tablice intro

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -3,12 +3,23 @@ package com.javacze;
 public class Main {
 
     public static void main(String[] args) {
-        int licznik = -1;
+        String[] uzytkownicy = new String[]{"Jan", "Aga", "Zenon", "Maria"};
+        String drugiUzytkownik = uzytkownicy[1];
 
-        do{
-            System.out.println("Hello: "+licznik);
-            licznik = licznik - 1; //licznik--;
-        }while(licznik > 0);
+        System.out.println(drugiUzytkownik);
+        System.out.println(uzytkownicy[1]);
+        System.out.println("");
+
+        drugiUzytkownik = "Beata";
+        System.out.println(drugiUzytkownik);
+        System.out.println(uzytkownicy[1]);
+        System.out.println("");
+
+        uzytkownicy[1] = "Alina";
+        System.out.println(drugiUzytkownik);
+        System.out.println(uzytkownicy[1]);
+        System.out.println("");
+
     }
 }
 
