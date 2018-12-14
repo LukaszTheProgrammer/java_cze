9d8511e07afb21ceeabe8f46163aefd2b380d220
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-14 12:23:56 +0100

    odczyt

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,10 +1,19 @@
 package com.javacze;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-        System.out.println("Hello World!");
+        File plik = new File("/Users/lukasz/plik.txt");
+        Scanner scanner = new Scanner(plik);
+
+        while(scanner.hasNext()){
+            System.out.println(scanner.nextLine());
+        }
     }
 }
 
