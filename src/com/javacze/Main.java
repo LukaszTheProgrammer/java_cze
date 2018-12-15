6d17e9fb0361061918cb840d5622fe481d8e97fc
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 15:27:29 +0100

    zapis z appendem

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,13 +1,21 @@
 package com.javacze;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        PrintWriter printWriter = new PrintWriter("/Users/lukasz/plik_do_zapisu.txt");
-        printWriter.append("Zapisujemy pierwszą linię");
+        //true jako drugi parametr do FileWriter'a powoduje że nasz plik jest otwarty
+        //w trybie 'append' co rozumie się jako dopisz a nie zastąp
+        FileWriter fileWriter = new FileWriter("/Users/lukasz/plik_do_zapisu.txt", true);
+        //Klasa writera która odpowiada za buforowanie zapisu do pliku
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        //PrintWriter pisze do pliku wykorzystując klasy BufferedWritera oraz FileWritera
+        PrintWriter printWriter = new PrintWriter(bufferedWriter);
+        printWriter.println("Zapisujemy pierwszą linię");//zapisz nową linię
         printWriter.close();
     }
 }
