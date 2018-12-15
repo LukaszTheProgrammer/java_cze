46fb4d192175eeae228b8925ae2ddc0108164492
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 12:12:55 +0100

    tablice

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,24 +1,51 @@
 package com.javacze;
 
+class Osoba {
+    String imie;
+    String nazwisko;
+
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Osoba{");
+        sb.append("imie='").append(imie).append('\'');
+        sb.append(", nazwisko='").append(nazwisko).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
 public class Main {
 
     public static void main(String[] args) {
+
+        // zmienna ktora mówi o rozmiarze tablic uzytkownicy.length;
+
+        // przyklad odnoszenia sie do poszczegolnych elementów System.out.println(uzytkownicy[i]);
+
+
         String[] uzytkownicy = new String[]{"Jan", "Aga", "Zenon", "Maria"};
-        String drugiUzytkownik = uzytkownicy[1];
+        for(int i=0 ; i < uzytkownicy.length; ++i){
+            System.out.println(uzytkownicy[i]);
+        }
 
-        System.out.println(drugiUzytkownik);
-        System.out.println(uzytkownicy[1]);
-        System.out.println("");
+        int [] liczby = new int[]{1,2,45,67,80,34,90,23};
+        for(int i=0 ; i < liczby.length; ++i){
+            System.out.println(liczby[i]);
+        }
 
-        drugiUzytkownik = "Beata";
-        System.out.println(drugiUzytkownik);
-        System.out.println(uzytkownicy[1]);
+        boolean [] bools = new boolean[]{true,false,false};
+        for(int i=0 ; i < bools.length; ++i){
+            System.out.println(bools[i]);
+        }
         System.out.println("");
-
-        uzytkownicy[1] = "Alina";
-        System.out.println(drugiUzytkownik);
-        System.out.println(uzytkownicy[1]);
-        System.out.println("");
+        Osoba [] ludzie = new Osoba[]{new Osoba("Jan", "Kowalski"), new Osoba("Tomek", "Nowak")};
+        for(int i=0 ; i < ludzie.length; ++i){
+            System.out.println(ludzie[i]);
+        }
 
     }
 }
