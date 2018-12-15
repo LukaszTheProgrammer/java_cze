7e06794180c119d8ab8c79650048343a919b04f6
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 22:43:46 +0100

    enum

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,97 +1,30 @@
 package com.javacze;
 
-interface Pojazd {
+enum Compass{
+    NORTH("N"), SOUTH("S"), WEST("W"), EAST("E");
 
-    void przyspiesz();
-
-    void hamuj();
-}
-
-class Bmw implements Pojazd {
-
-    private static final int przyspieszenie = 10;
-    private static final int topSpeed = 250;
-
-    private int predkosc = 0;
-
-    @Override
-    public void przyspiesz() {
-        if (predkosc < topSpeed) {
-            predkosc += przyspieszenie;
-        }
+    Compass(String symbol){
+        this.symbol = symbol;
     }
 
-    @Override
-    public void hamuj() {
-        if (predkosc > 0) {
-            predkosc -= przyspieszenie;
-        }
-    }
+    private String symbol;
 
-    @Override
-    public String toString() {
-        return "Bmw{" +
-            "predkosc=" + predkosc +
-            '}';
-    }
-}
-
-class Rower implements Pojazd {
-
-    private static final int przyspieszenie = 2;
-    private static final int topSpeed = 50;
-    private static final int wspolczynnikHamowania = 3;
-
-    private int predkosc = 0;
-
-    @Override
-    public void przyspiesz() {
-        if (predkosc < topSpeed) {
-            predkosc += przyspieszenie;
-        }
-    }
-
-    @Override
-    public void hamuj() {
-        if (predkosc > 0) {
-            predkosc -= wspolczynnikHamowania;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Rower{" +
-            "predkosc=" + predkosc +
-            '}';
+    public String getSymbol() {
+        return symbol;
     }
 }
 
 public class Main {
 
     public static void main(String[] args) {
-        Pojazd bmw = new Bmw();
-        Pojazd rower = new Rower();
+        Compass direction = Compass.SOUTH;
 
-        symuluj(bmw, rower);
-    }
+        Compass direction2 = Compass.valueOf("NORTH");
 
-    private static void symuluj(Pojazd... pojazdy) {
-
-        for (int i = 0; i < 5; i++) {
-            for (Pojazd pojazd : pojazdy) {
-                pojazd.przyspiesz();
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            for (Pojazd pojazd : pojazdy) {
-                pojazd.hamuj();
-            }
-        }
-
-        for (Pojazd pojazd: pojazdy){
-            System.out.println(pojazd);
+        for (Compass d: Compass.values()) {
+            System.out.println(d+" "+d.getSymbol());
         }
     }
+
 }
 
