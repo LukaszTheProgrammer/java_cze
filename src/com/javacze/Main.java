275ec9087a735d12153d718471ae46658f71897a
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 15:53:01 +0100

    zapis z wykorzystaniem inputu od uzytkownika

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,22 +1,44 @@
 package com.javacze;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Proszę podać nazwę pliku");
+        String filename = scanner.nextLine();
+
         //true jako drugi parametr do FileWriter'a powoduje że nasz plik jest otwarty
         //w trybie 'append' co rozumie się jako dopisz a nie zastąp
-        FileWriter fileWriter = new FileWriter("/Users/lukasz/plik_do_zapisu.txt", true);
+        String filePath = "/Users/lukasz/"+filename;
+        FileWriter fileWriter = new FileWriter(filePath, true);
         //Klasa writera która odpowiada za buforowanie zapisu do pliku
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         //PrintWriter pisze do pliku wykorzystując klasy BufferedWritera oraz FileWritera
         PrintWriter printWriter = new PrintWriter(bufferedWriter);
-        printWriter.println("Zapisujemy pierwszą linię");//zapisz nową linię
-        printWriter.close();
+
+        while(true){
+            System.out.println("Proszę o linię");
+            String linia = scanner.nextLine();
+            if(linia.equalsIgnoreCase("!zapisz")){
+                printWriter.close();
+                break;
+            } else{
+                printWriter.println(linia);
+            }
+        }
+
+        Scanner fileReader = new Scanner(new File(filePath));
+        while(fileReader.hasNext()){
+            System.out.println(fileReader.nextLine());
+        }
+
     }
 }
 
