3c9afffe6cc8bdfc76388853790dfbf268b92b95
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 14:24:41 +0100

    tablice2d-zadanie

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -4,40 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] macierz1 = new int[3][3];
-        int[][] macierz2 = new int[][]{{1,2,3}, {4,5,6}, {7,8,9}};
-        int[][] macierz3 = new int[][]{{1,2}, {4,5,6}, {7}};
+        int[][] macierz = new int[5][5];
 
-        System.out.println("Listowanie macierzy1:");
-        for (int i=0; i< macierz1.length; ++i){
-            System.out.print("Rozmiar wiersza numer "+i+": "+macierz1[i].length+" zawartość: ");
-            for(int k=0; k< macierz1[i].length; ++k){
-                System.out.print(macierz1[i][k]+" ");
+        for (int i = 0; i < macierz.length; ++i) {
+            for (int k = 0; k < macierz[i].length; ++k) {
+                if (i == 2) {
+                    macierz[i][k] = 1;
+                } else {
+                    macierz[i][k] = 0;
+                }
+                System.out.print(macierz[i][k] + " ");
             }
-            System.out.println("");
+            System.out.print("\n\t tekst \n");
         }
-        System.out.println("");
-
-        System.out.println("Listowanie macierzy2:");
-        for (int i=0; i< macierz2.length; ++i){
-            System.out.print("Rozmiar wiersza numer "+i+": "+macierz2[i].length+" zawartość: ");
-            for(int k=0; k< macierz2[i].length; ++k){
-                System.out.print(macierz2[i][k]+" ");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-
-        System.out.println("Listowanie macierzy3:");
-        for (int i=0; i< macierz3.length; ++i){
-            System.out.print("Rozmiar wiersza numer "+i+": "+macierz3[i].length+" zawartość: ");
-            for(int k=0; k< macierz3[i].length; ++k){
-                System.out.print(macierz3[i][k]+" ");
-            }
-            System.out.println("");
-        }
-        System.out.println("");
-
     }
 }
 
