d382bc34229a439ad1084e720a2ca386648b7238
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-15 13:23:40 +0100

    zadanie z imionami

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,56 +1,21 @@
 package com.javacze;
 
-class Osoba {
+import java.util.Scanner;
 
-    private String imie;
-    private String nazwisko;
-
-    Osoba(String imie, String nazwisko) {
-        this.imie = imie;
-        this.nazwisko = nazwisko;
-    }
-
-    @Override
-    public String toString() {
-        return "Osoba{" + "imie='" + imie + '\''
-            + ", nazwisko='" + nazwisko + '\''
-            + '}';
-    }
-}
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        // zmienna ktora mówi o rozmiarze tablic uzytkownicy.length;
-
-        // przyklad odnoszenia sie do poszczegolnych elementów System.out.println(uzytkownicy[i]);
-
-
-        String[] uzytkownicy = new String[]{"Jan", "Aga", "Zenon", "Maria"};
-        for(int i=0 ; i < uzytkownicy.length; ++i){
-            System.out.println(uzytkownicy[i]);
-        }
-        System.out.println("");
-
-        for(int i=uzytkownicy.length-1 ; i >= 0; --i){
-            System.out.println(uzytkownicy[i]);
-        }
-        System.out.println("");
-
-        int [] liczby = new int[]{1,2,45,67,80,34,90,23};
-        for(int i=0 ; i < liczby.length; ++i){
-            System.out.println(liczby[i]);
+        int rozmiarTablicy = 5;
+        String[] imiona = new String[rozmiarTablicy];
+        for (int i = 0; i < rozmiarTablicy; i++) {
+            System.out.println("Podaj imię:");
+            imiona[i] = scanner.nextLine();
         }
 
-        boolean [] bools = new boolean[]{true,false,false};
-        for(int i=0 ; i < bools.length; ++i){
-            System.out.println(bools[i]);
-        }
-        System.out.println("");
-
-        Osoba [] ludzie = new Osoba[]{new Osoba("Jan", "Kowalski"), new Osoba("Tomek", "Nowak")};
-        for(int i=0 ; i < ludzie.length; ++i){
-            System.out.println(ludzie[i]);
+        for (String imie: imiona){
+            System.out.println("Witaj, "+imie);
         }
     }
 }
