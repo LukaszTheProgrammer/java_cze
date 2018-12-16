8268918ad28ba28129071bec68f7a3e1c1ec9e70
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-16 09:43:41 +0100

    zadanie

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -3,8 +3,17 @@ package com.javacze;
 public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello World!");
+        //long startTime = System.currentTimeMillis();
+        long suma = 0l;
+        for (int i=0; i< 1000; ++i){
+            if(i%3==0 || i%5==0){
+                suma += i;
+            }
+        }
+        //long endTime = System.currentTimeMillis();
+        //long czasRozwiazania = endTime - startTime;
+        System.out.println(suma);
+        //System.out.println("Całość zajęła "+czasRozwiazania+" ms");
     }
 }
 
