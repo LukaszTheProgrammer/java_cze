cf289f9e7778111e3a13e54e7014b5aeff7a8775
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-16 11:44:48 +0100

    program

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -2,18 +2,35 @@ package com.javacze;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        File plik = new File("/Users/lukasz/plik.txt");
+        File plik = new File("/Users/lukasz/do_korekty.txt");
         Scanner scanner = new Scanner(plik);
-
-        while(scanner.hasNext()){  // zwraca true jeśli w buforze danych znajdują się dane
-            System.out.println(scanner.nextLine());
+        PrintWriter pw = new PrintWriter("/Users/lukasz/poprawiony.txt");
+        while(scanner.hasNext()){  // zwraca true jeśli w buforze danych znajdują
+            // się dane
+            String line = scanner.nextLine().trim(); //czytamy linię z pliku
+            // i wycinamy z niej wiodące i kończące białe znaki
+            String firstChar = line.substring(0,1); //zwracamy String będący pierwszym
+            // znakiem
+            //toUpperCase zwraca napis w którym wszystkie znaki są zamienione na
+            // ich duże odpowiedniki
+            //w rezultacie dodajemy pierwszy znak zamieniony na duży do reszty linii
+            String res = firstChar.toUpperCase() + line.substring(1, line.length());
+            if(!res.endsWith(".")){ //jeśli napis nie kończy się kropką
+                res = res + "."; //dodajemy kropkę
+            }
+            pw.println(res);
+            System.out.println(res);
         }
+        pw.close();
+        scanner.close();
     }
+
 }
 
