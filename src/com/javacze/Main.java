5ce5063ce799f9c54a353d7e5597a122d2d1abb9
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-16 13:44:45 +0100

    program

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -14,11 +14,21 @@ enum Day {
     Day(String nazwaSkrocona) {
         this.nazwaSkrocona = nazwaSkrocona;
     }
+
+    public static Day of(String p){
+        for (Day zmienna : Day.values()){
+            if(zmienna.toString().equals(p)){
+                return zmienna;
+            }
+        }
+        throw new IllegalArgumentException("Nie znaleziono Day::"+p);
+    }
 }
 
 public class Main {
 
     public static void main(String[] args) {
+        // Day.valueOf(String) == Day.of(String)
         for (Day zmienna : Day.values()) {
             if(zmienna == Day.SOBOTA || zmienna == Day.NIEDZIELA){
                 System.out.println("Jest Weekend!");
@@ -26,9 +36,27 @@ public class Main {
             System.out.println(zmienna);
         }
         System.out.println("");
+
         for (Compass zmienna : Compass.values()) {
             System.out.println(zmienna);
         }
+        /*
+        złe inicjalizacje enum'a typu Day. Możliwe tylko wartości zdefioniowane
+        w enumie Day
+
+        Day z = 1;
+        Day z2 = "";
+        Day z3 = boolean;
+        Day z4 = 'a';
+        */
+
+
+        Day zmienna = Day.valueOf("WTOREK");
+        Day zmienna2 = Day.of("PIATEK");
+        //Day zmienna2 = Day.valueOf("PON"); błąd! nazwa stałej musi się zgadzać 1 do 1 z nazwą podaną do valueOf
+        System.out.println(zmienna2);
+
+
 
     }
 
