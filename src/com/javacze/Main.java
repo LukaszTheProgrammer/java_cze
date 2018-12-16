baad4f58fa7bd9aebebfcbc66ef1f8729a39098d
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-16 10:01:49 +0100

    zadanie

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -3,17 +3,41 @@ package com.javacze;
 public class Main {
 
     public static void main(String[] args) {
-        //long startTime = System.currentTimeMillis();
+        rozwiazanieNaiwne(100000000);
+        rozwiazanieUlepszone(100000000);
+    }
+
+    private static void rozwiazanieNaiwne(int rozmiarZadania) {
+        long startTime = System.currentTimeMillis();
         long suma = 0l;
-        for (int i=0; i< 1000; ++i){
+        for (int i=0; i< rozmiarZadania; ++i){
             if(i%3==0 || i%5==0){
                 suma += i;
             }
         }
-        //long endTime = System.currentTimeMillis();
-        //long czasRozwiazania = endTime - startTime;
         System.out.println(suma);
-        //System.out.println("Całość zajęła "+czasRozwiazania+" ms");
+        long endTime = System.currentTimeMillis();
+        long czasRozwiazania = endTime - startTime;
+        System.out.println("Całość zajęła "+czasRozwiazania+" ms");
+    }
+
+    private static void rozwiazanieUlepszone(int rozmiarZadania) {
+        long startTime = System.currentTimeMillis();
+        long suma = 0l;
+        for (int i=1; (i*3)< rozmiarZadania; ++i){
+                suma += i*3;
+        }
+
+        for (int i=1; (i*5)< rozmiarZadania; ++i){
+            if(!(i%3==0)) {
+                suma += i * 5;
+            }
+        }
+        long endTime = System.currentTimeMillis();
+
+        System.out.println(suma);
+        long czasRozwiazania = endTime - startTime;
+        System.out.println("Całość zajęła "+czasRozwiazania+" ms");
     }
 }
 
