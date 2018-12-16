4c256837e9a53cd741ccafd6651652d98b744030
Lukasz Odulinski <lukasz@videolicious.com>
2018-12-16 15:43:03 +0100

    zadanie

--- src/com/javacze/Main.java
+++ src/com/javacze/Main.java
@@ -1,20 +1,22 @@
 package com.javacze;
 
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        Scanner scanner = new Scanner(new File("/Users/lukasz/liczby_hex.txt"));
-        while (scanner.hasNext()) {
-            String liczbaHex = scanner.next(); //czytamy kolejne liczby
-            Integer liczba = Integer.valueOf(liczbaHex, 16);//konwersja ze Stringa w postaci 16-tkowej do Integera
-            System.out.println(liczbaHex + " -> " + liczba);//drukowanie na ekranie wartości liczbowej
-            // w systemie 16-tkowym i jej odpowiednika w systemie 10-tnym
+        printMany("jeden");
+        printMany("jeden","dwa");
+        printMany("jeden", "dwa", "trzy");
+        printMany();
+    }
+
+    private static void printMany(String... strings) {
+        for(String string: strings){
+            System.out.print(string+" ");
         }
+        System.out.println("");
     }
 }
 
